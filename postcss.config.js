0b0a5441764ee92ef28900827b432395a6d211ec
João Pedro Limão <jplimao12@gmail.com>
2020-06-24 14:12:38 -0300

    Update package.json and postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
     // Specify the paths to all of the template files in your project
     content: [
-      './index.html',
+      'core/templates/login.html',
     //   './src/**/*.vue',
     //   './src/**/*.jsx',
       // etc.
