a65d8618844311f28f40cd309576fabdd40edced
João Pedro Limão <jplimao12@gmail.com>
2020-07-04 18:44:09 -0300

    feature: Confing python-decouple

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ze#g5b)l!#7-uy^_h(fjilb!aos3v=p3bc0=jkeii_x-_qlzx='
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
