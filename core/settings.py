9d6b52624f628d5314845915bc0d883870d110a3
João Pedro Limão <jplimao12@gmail.com>
2020-07-25 22:58:30 -0300

    style: Adding rest_framework in serttings.py

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
-    'social_django'
+    'social_django',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
