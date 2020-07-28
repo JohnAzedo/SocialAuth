37f0290cda69558db0cabf106a54b4e382602dbb
João Pedro Limão <jplimao12@gmail.com>
2020-07-28 13:11:15 -0300

    feat: Import social_django urls

--- authentication/env.py
+++ authentication/env.py
@@ -2,7 +2,7 @@ from decouple import config
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_CLIENT_ID')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_CLIENT_SECRET')
-LOGIN_URL = '/auth/login/google-oauth2/'
+LOGIN_URL = '/login/google-oauth2/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_URL_NAMESPACE = 'authentication'
