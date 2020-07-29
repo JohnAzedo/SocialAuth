45c01a166336289392aeaf29285d8050dc822eac
JohnAzedo <jplimao12@gmail.com>
2020-07-28 21:13:24 -0300

    fix: Handling env error

--- authentication/env.py
+++ authentication/env.py
@@ -1,7 +1,11 @@
 from decouple import config
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_CLIENT_ID')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_CLIENT_SECRET')
+try:
+    SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_CLIENT_ID')
+    SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_CLIENT_SECRET')
+except ValueError:
+    print('Google OAuth2 no variables')
+    
 LOGIN_URL = '/login/google-oauth2/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
